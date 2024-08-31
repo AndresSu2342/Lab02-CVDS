fd6fe6e985ad97b58b2a0ba70eec37ce447c4b85
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2024-08-30 21:47:26 -0500

    Update App.java

--- Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -1,15 +1,40 @@
 package edu.eci.cvds.patterns.archetype;
 
-public class App 
-{
-    public static void main(String[] args)
-    {
-        if (args.length >= 2) {
-            System.out.println("Hola, " + args[0] + " " + args[1] + "!");
-        } else if (args.length == 1) {
-            System.out.println("Hola, " + args[0] + "!");
+import edu.eci.cvds.patterns.shapes.Shape;
+import edu.eci.cvds.patterns.shapes.concrete.Hexagon;
+import edu.eci.cvds.patterns.shapes.concrete.Pentagon;
+import edu.eci.cvds.patterns.shapes.concrete.Quadrilateral;
+import edu.eci.cvds.patterns.shapes.concrete.Triangle;
+
+public class App {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Please provide the name of a shape (e.g., Hexagon, Pentagon, Quadrilateral, Triangle).");
+            return;
+        }
+
+        String shapeName = args[0];
+        Shape shape = createShape(shapeName);
+
+        if (shape != null) {
+            System.out.println("The number of edges of " + shapeName + " is: " + shape.getNumberOfEdges());
         } else {
-            System.out.println("Hola Mundo!");
+            System.err.println("Unknown shape: " + shapeName);
+        }
+    }
+
+    private static Shape createShape(String shapeName) {
+        switch (shapeName.toLowerCase()) {
+            case "hexagon":
+                return new Hexagon();
+            case "pentagon":
+                return new Pentagon();
+            case "quadrilateral":
+                return new Quadrilateral();
+            case "triangle":
+                return new Triangle();
+            default:
+                return null;
         }
     }
 }
