a6e5de760eb28928bbda1dcd1ce982e53c217556
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2024-08-30 21:49:24 -0500

    Update App.java

--- Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -9,7 +9,8 @@ import edu.eci.cvds.patterns.shapes.concrete.Triangle;
 public class App {
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.err.println("Please provide the name of a shape (e.g., Hexagon, Pentagon, Quadrilateral, Triangle).");
+            System.err.println("Usage: java App <shape-name>");
+            System.err.println("Example: java App Hexagon");
             return;
         }
 
@@ -20,6 +21,7 @@ public class App {
             System.out.println("The number of edges of " + shapeName + " is: " + shape.getNumberOfEdges());
         } else {
             System.err.println("Unknown shape: " + shapeName);
+            System.err.println("Please provide one of the following shapes: Hexagon, Pentagon, Quadrilateral, Triangle.");
         }
     }
 
